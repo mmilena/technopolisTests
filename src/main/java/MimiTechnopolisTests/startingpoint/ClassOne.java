f5eaf1cb53f51242a968f1f883f64e9029c6ae6a
Milena Atanasova <atanasowa.m@gmail.com>
2016-12-03 21:59:45 +0200

    First commint

---
+++ src/main/java/MimiTechnopolisTests/startingpoint/ClassOne.java
@@ -0,0 +1,26 @@
+package MimiTechnopolisTests.startingpoint;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ClassOne {
+	WebDriver driver;
+	
+	@Before
+	public void setUpEnvironment(){
+		System.setProperty("webdriver.chrome.driver", "D://libr//chromedriver.exe");
+		 driver = new ChromeDriver();
+		 driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		 
+	}
+	
+	@Test
+	public void loadPage(){
+		driver.get("http://www.technopolis.bg/");
+	}
+
+}
